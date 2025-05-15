646090c14be5dcb72a7cfad25d18e33b3bb2076b
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-05-15 19:56:57 +0530

    appwrite setup complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+ 
+ console.log(import.meta.env.VITE_APPWRITE_URL);
+ 
+  return (
+    <>
+      <h1 className='text-orange-500 text-2xl bg-black p-4 rounded-2xl'>LateNightWrites</h1>
+    </>
+  )
+}
+
+export default App
