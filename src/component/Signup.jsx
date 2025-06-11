c5f720133f2ddc0fdac6983c59b0f08d9eca9b6f
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-06-11 20:07:13 +0530

    authLayout component

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,36 +1,33 @@
 import React, { useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
-import { login as storeLogin } from "../store/authSlice";
-import { Button, Input, Logo } from "../component/index";
 import { useDispatch } from "react-redux";
-import authService from "../appwrite/auth/auth";
 import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Logo, Input } from "../component/index";
+import authService from "../appwrite/auth/auth";
+import { login as storeLogin } from "../store/authSlice";
 
-function Login() {
+function Signup() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const [error, setError] = useState("");
   const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
 
-  const login = async (data) => {
-    // always empty out error before submission
+  const create = async (data) => {
     setError("");
-
     try {
-      const session = await authService.login(data);
-      if (session) {
-        // userData is coming from authService not from session
-        const userData = await authService.getCurrentUser();
-        if (userData) dispatch(storeLogin(userData));
+      const userData = await authService.createAccount(data);
+
+      if (userData) {
+        const useData = await authService.getCurrentUser();
+        if (userData) dispatch(login(userData));
         navigate("/");
       }
     } catch (error) {
       setError(error.message);
     }
   };
-
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center">
       <div
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
@@ -40,18 +37,27 @@ function Login() {
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sign in to your account
+          Sign up to create account
         </h2>
         <p className="mt-2 text-center text-base text-black/60">
-          Don&apos;t have any account?&nbsp;
+          Already have an account?&nbsp;
           <Link
-            to={"/signup"}
+            to="/login"
             className="font-medium text-primary transition-all duration-200 hover:underline"
-          ></Link>
+          >
+            Sign In
+          </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(login)} className="mt-8">
+        <form onSubmit={handleSubmit(create)}>
           <div className="space-y-5">
+            <Input
+              label="Full Name: "
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
             <Input
               label="Email :"
               placeholder="Enter your email"
@@ -66,22 +72,17 @@ function Login() {
               })}
             />
             <Input
-              label="Password: "
-              placeholder="Enter your password"
-              type="password"
-              {...register("password", {
-                required: true,
-                validate: {
-                  matchPattern: (value) =>
-                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(
-                      value
-                    ) || "Enter password must be a valid password",
-                },
-              })}
+            label="Password: "
+            placeholder="Enter your password"
+            type="password"
+            {...register("password",{
+                required:true,
+                validate:{
+                    matchPattern: (value) => /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(value) || "Enter valid password"
+                }
+            })}
             />
-            <Button type="submit" className="w-full">
-              Sign in
-            </Button>
+            <Button type="submit" className="w-full">Create Account</Button>
           </div>
         </form>
       </div>
@@ -89,4 +90,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
